ce465c234e4fb10072477e8c3a632996d4d83ca1
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-05-11 11:40:31 -0300

    Integration tests description corrections

--- src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -7,7 +7,7 @@ import createConnection from "../../../../database";
 
 let connection: Connection;
 
-describe("Create user controller", () => {
+describe("Authenticate user controller", () => {
   beforeAll(async () => {
     authConfig.jwt.secret = "335cd5e290807fd304c6b635e7cb0c5c";
     connection = await createConnection();
