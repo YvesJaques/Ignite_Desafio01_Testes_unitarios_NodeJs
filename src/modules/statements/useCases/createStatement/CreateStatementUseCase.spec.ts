97c6a88d870ac8db0bf40fbe14e2949d31089412
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-05-24 08:54:50 -0300

    Create statement use case unit tests
    Added transfer operation test

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -6,7 +6,6 @@ import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMem
 import { GetBalanceUseCase } from "../getBalance/GetBalanceUseCase";
 import { OperationType } from "./CreateStatementController";
 import { CreateStatementUseCase } from "./CreateStatementUseCase";
-import { ICreateStatementDTO } from "./ICreateStatementDTO";
 
 let usersRepositoryInMemory: InMemoryUsersRepository;
 let createUserUseCase: CreateUserUseCase;
@@ -122,5 +121,56 @@ describe("Create statement", () => {
         description: "Withdraw test",
       });
     }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should be able to transfer credits from one user to another", async () => {
+    const user1: ICreateUserDTO = {
+      name: "Cody Daniel",
+      email: "soowlo@zuf.ms",
+      password: "1234",
+    };
+
+    const user1_id = <string>(await createUserUseCase.execute(user1)).id;
+
+    const user2: ICreateUserDTO = {
+      name: "Clara Cortez",
+      email: "test@mail.com",
+      password: "1234",
+    };
+
+    const user2_id = <string>(await createUserUseCase.execute(user2)).id;
+
+    await createStatementUseCase.execute({
+      user_id: user1_id,
+      type: "deposit" as OperationType,
+      amount: 100,
+      description: "Deposit test",
+    });
+
+    const statement = {
+      user_id: user2_id,
+      sender_id: user1_id,
+      type: "transfer" as OperationType,
+      amount: 100,
+      description: "Transfer test",
+    };
+
+    const createdStatement = await createStatementUseCase.execute(statement);
+
+    const user1_balance = await statementRepositoryInMemory.getUserBalance({
+      user_id: user1_id,
+    });
+
+    const user2_balance = await statementRepositoryInMemory.getUserBalance({
+      user_id: user2_id,
+    });
+
+    expect(user1_balance.balance).toEqual(0);
+    expect(user2_balance.balance).toEqual(100);
+    expect(createdStatement).toHaveProperty("type", statement.type);
+    expect(createdStatement).toHaveProperty("type", statement.type);
+    expect(createdStatement).toHaveProperty("amount", statement.amount);
+    expect(createdStatement).toHaveProperty("user_id", user2_id);
+    expect(createdStatement).toHaveProperty("sender_id", user1_id);
   });
 });
